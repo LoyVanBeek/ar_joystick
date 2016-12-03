78a6cca929076c811e2dd98aea04606581d49d6e
Loy van Beek <loy.vanbeek@gmail.com>
2016-12-03 17:29:26 +0100

    Use the correct frames in the transform

--- src/ar_joystick.py
+++ src/ar_joystick.py
@@ -18,17 +18,19 @@ def transform_to_joy(translation, rotation):
 if __name__ == "__main__":
     rospy.init_node("ar_joy_node")
 
+    ORIGIN, TARGET = "/usb_cam", "/ar_marker1"
+
     joy_publisher = rospy.Publisher("/joy", sensor_msgs.msg.Joy, queue_size=100)  # Publisher for Joystick messages
 
     listener = tf.TransformListener()  # Listen to messages on the /tf topic and construct a tranformation tree that can be queried
 
     rate = rospy.Rate(10.0)  # What frequency runs our node on, roughly
 
-    listener.waitForTransform('/world', '/usb_cam', rospy.Time(0), rospy.Duration(1))
+    listener.waitForTransform(ORIGIN, TARGET, rospy.Time(0), rospy.Duration(1))
 
     while not rospy.is_shutdown():  # ctrl-C makes this return True
         try:
-            (trans,rot) = listener.lookupTransform('/world', '/usb_cam', rospy.Time(0))  # What is the transformation between these frames at the nearest time
+            (trans,rot) = listener.lookupTransform(ORIGIN, TARGET, rospy.Time(0))  # What is the transformation between these frames at the nearest time
             joy_msg = transform_to_joy(trans, rot)
 
             joy_publisher.publish(joy_msg)
